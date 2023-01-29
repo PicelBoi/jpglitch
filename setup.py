2156cff60e4c28b9db125a47d416b5042fefa984
Dan <31395415+cakedan@users.noreply.github.com>
2023-01-28 23:43:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,4 @@ setup(
     name='jpglitch',
     version='0.2',
     py_modules=['jpglitch'],
-    install_requires=[
-        'Click',
-        'Pillow',
-    ],
-    entry_points='''
-        [console_scripts]
-        jpglitch=jpglitch:cli
-    ''',
 )
